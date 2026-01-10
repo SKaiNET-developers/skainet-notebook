e6c40ce77db1dcf850dbb59a11b84af0ac6cc671
Michal Harakal <michal.harakal@googlemail.com>
2026-01-10 13:32:52 +0100

    Re-export SKaiNET classes als typealias in sk.ainet.app.notebook package

---
+++ kotlin-notebook/src/main/kotlin/sk/ainet/app/notebook/NotebookAliases.kt
@@ -0,0 +1,15 @@
+package sk.ainet.app.notebook
+
+
+// execution
+typealias ExecutionContext = sk.ainet.context.ExecutionContext
+typealias DirectCpuExecutionContext = sk.ainet.context.DirectCpuExecutionContext
+
+// tensor
+typealias Shape = sk.ainet.lang.tensor.Shape
+typealias Tensor<T, V> = sk.ainet.lang.tensor.Tensor<T, V>
+
+// data + types
+typealias DenseTensorDataFactory = sk.ainet.lang.tensor.data.DenseTensorDataFactory
+typealias FP32 = sk.ainet.lang.types.FP32
+
